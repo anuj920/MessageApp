ebfd5d7dff4a3c52b68e78cd64968232820bacf7
Anuj Singh <anujk920@gmail.com>
2019-12-17 04:32:09 +0530

    Add API Login, Register, Logout and Token Auth

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     # url(r'^api_message$',views.MessageView.as_view(),name='api_message'),
     path("AddMessage/",views.AddMessage.as_view(),name = 'AddMessage'),
     url(r'Message/(?P<pk>[0-9]+)/$', views.MessageOperation.as_view(), name='Message'),
+    path("Register/",views.Register.as_view(),name = 'Register'),
+    path("Login/",views.Login.as_view(),name = 'Login'),
+    path("Logout/",views.Logout.as_view(),name = 'Logout'),
 ]
 
--- api/views.py
+++ api/views.py
@@ -7,6 +7,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from rest_framework.generics import DestroyAPIView
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
 
 
 
@@ -39,5 +43,53 @@ class MessageOperation(generics.RetrieveUpdateDestroyAPIView):
         self.perform_update(serializer)
         return Response(serializer.data)
 
+class Register(APIView):
 
+    def post(self, request):
+        password1 = request.data.get("password")
+        password2 = request.data.get("confirm_password")
+        email = request.data.get("email")
+        user = request.data.get("user")
+        if password1 and password2 and password2 == password1:
+            if user:
+                user_exists = User.objects.filter(username=user).exists()
+                if user_exists:
+                    return Response({"message": "User with this User Name already exists.", "flag": False},status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    user = User.objects.create_user(username=user, password=password1, email=email)
+                    user.is_active = False
+                    user.save()
+                    return Response({"message": "User Register Successfully"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "Please check the form filled."}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "Please fill Details"}, status=status.HTTP_201_CREATED)
+
+
+class Logout(DestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.auth
+
+
+class Login(APIView):
+    
+    def post(self, request):
+        name = request.data.get("username")
+        password = request.data.get("password")
+        # print(name,password)
+        try:
+            user_exists = User.objects.filter(username=name)
+            if not user_exists.exists():
+                return Response({"message": "User with this details not exists.", "flag": False},status=status.HTTP_400_BAD_REQUEST)
+            user_obj = authenticate(username=user_exists[0].username, password=password)
+            if user_obj:
+                if user_obj.is_active:
+                    user_token, created=Token.objects.get_or_create(user=user_obj)
+                    return Response({"message":"Logged in","username":user_obj.username,"token":user_token.key},status=status.HTTP_200_OK)
+            else:
+                return Response({"message": 'Password Incorrect', "flag": False}, status=status.HTTP_401_UNAUTHORIZED)
+        except Exception as e:
+            return Response(
+                {'message': 'Please enter a valid username and password.', "details": str(e), "flag": False},status=status.HTTP_401_UNAUTHORIZED)
 
